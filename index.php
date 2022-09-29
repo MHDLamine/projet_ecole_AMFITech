dc1110092c6ee79aabe17301c1a68831d62670ea
fadal ba <fadalba@gmail.com>
2022-09-29 16:07:50 +0000

    logo

--- index.php
+++ index.php
@@ -7,8 +7,14 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Ecole de la Réussite</title>
+    <img src="" alt="logo_page">
 </head>
 <body>
+    <!--intégration du php-->
+    
+    <?php
+
+    ?>
     
 </body>
 </html>

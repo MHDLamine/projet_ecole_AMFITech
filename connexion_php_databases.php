a14e4a27303fb7fdcd47fd98031c058aab2fe199
Adama Dieye <adamadieye780@gmail.com>
2022-10-05 17:57:30 +0000

    connexion

--- connexion_php_databases.php
+++ connexion_php_databases.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "gahdamns";
 $dbname = "Gestion_ecole";
 
 try {
--- inscription/inscription.php
+++ inscription/inscription.php
@@ -52,7 +52,7 @@ else {
 <body>
 
 
-<?php include("index.php"); ?>
+
 
 <body>
 <div class="container">

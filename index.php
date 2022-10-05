b85f5365eb2988465e3fe551a122647d4adbdcb9
fadal ba <fadalba@gmail.com>
2022-10-05 15:07:06 +0000

    mises_à_jour_2

--- index.php
+++ index.php
@@ -6,17 +6,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- CSS only -->
-   
-    <link rel="stylesheet" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="acceuil_style.css">
+    <link rel="stylesheet" href="acceuil_style.css">
     <title>Ecole de la Réussite</title>
-    <img src="./img/logo-lareussite.png " alt="logo_page" >
+    <img src="./img/logo-lareussite.png " alt="logo_page" width = 100px >
 </head>
 <body>
+    <div class="container">
  <!-- ************************************Nav bar******************************-->
-
-    <!--intégration du php-->
+ <nav class="nav">
+  <a class="nav-link " aria-current="page" href="compte.php">Connexion</a>
+  <a class="nav-link" href="./inscription/inscription.php">Inscription</a>
+  <a class="nav-link" href="contact.php">Nous contacter</a>
+ 
+</nav>
   
-    <a href="inscription/inscription.php">inscription</a>
+</ul>
+    <!--*******************************************slider*****************************************-->
+    <div class="container">
+    <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./img/logo-lareussite.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="./img/ecole.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+</div>
+   
 
 <!-- JavaScript Bundle with Popper -->
 

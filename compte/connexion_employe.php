fe7e79a3f83888acb48893996af66c0c897aa0be
Adama Dieye <adamadieye780@gmail.com>
2022-10-05 17:34:38 +0000

    connexion

--- compte/connexion_employe.php
+++ compte/connexion_employe.php
@@ -1,3 +1,22 @@
+<?php include '../connexion_php_databases.php';?>
+
+<?php 
+
+    if(isset($_POST['adresse_mail_Employes']) && $_POST['mot_de_passe'])
+    {
+
+      $email = htmlspecialchars($_POST['adresse_mail_employes']);
+      $mdp = htmlspecialchars($_POST['mot_de_passe']);
+      $verif = $bdd -> prepare("SELECT adresse_mail_employes, mot_de_passe from employes where adresse_mail_employes = ?");
+      $verif -> execute(array('adresse_mail_employes'));
+      $data = $verif -> fetch();
+      $row = $verif-> rowCount();
+    }
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,16 +27,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="../style/c_employe.css">
-
 </head>
-
 <body>
-
     <div class="login">
 
         <h1 class="text-center">Bonjour</h1>
 
-        <form class="needs-validation">
+        <form method='post' class="needs-validation">
             <div class="form-group was-validated">
                 <label class="form-label" for="email">Adresse mail</label>
                 <input class="form-control" type="email" id="email" required>
@@ -27,7 +43,7 @@
                 <input class="form-control" type="password" id="password" required>
                
             </div>
-            <input class="btn btn-success w-100" type="submit" value="S'identifier">
+            <input class="btn" type="submit" value="S'identifier">
         </form>
 
     </div>
--- connexion_php_databases.php
+++ connexion_php_databases.php
@@ -2,14 +2,12 @@
 $servername = "localhost";
 $username = "root";
 $password = "gahdamns";
-$password = "";
 $dbname = "Gestion_ecole";
 try {
   $conn = new PDO("mysql:host=$servername;dbname=Gestion_ecole", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "";
-  echo " <h1>Bienvenu a la base de donnée de l'établissement</h1> <br>";
 } catch(PDOException $e) {
   echo "Connection échouée: " . $e->getMessage();
 }

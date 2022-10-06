b0e5052ac2cdd9e906543dd07894a7c23dc33267
Adama Dieye <adamadieye780@gmail.com>
2022-10-06 14:36:58 +0000

    connexion

--- compte/connexion_employe.php
+++ compte/connexion_employe.php
@@ -2,30 +2,30 @@
 
 <?php 
 
-    if(isset($_POST['adresse_mail_Employes']) && $_POST['mot_de_passe'])
+    if(isset($_POST['email']) && $_POST['mdp'])
     {
 
-      $email = htmlspecialchars($_POST['adresse_mail_employes']);
-      $mdp = htmlspecialchars($_POST['mot_de_passe']);
-      $verif = $bdd -> prepare("SELECT adresse_mail_employes, mot_de_passe from employes where adresse_mail_employes = ? AND mot_de_passe = ?");
-      $verif -> execute(array('adresse_mail_employes', 'mot_de_passe'));
-      $data = $verif -> fetch();
+      $email = htmlspecialchars($_POST['email']);
+      $mdp = htmlspecialchars($_POST['mdp']);
+      $verif = $conn -> query("SELECT adresse_mail_employes, mot_de_passe from employes where adresse_mail_employes='$email' AND mot_de_passe='$mdp'");
 
-      if (!filter_var($email, FILTER_VALIDATE_EMAIL) || $mdp != $data["mot_de_passe"])
-      {
-          echo 'mail ou Mot De Passe incorrect.<br/>';
-      }
-      else
-      {
-          echo 'Vous êtes connecté ! :-)<br/>';
-      }
-      $req->closeCursor();
-  }
-  else
-  {
-      echo 'Renseignez un mail et un Mot De Passe.<br/>';
-  }
-  
+if($verif->rowCount() > 0) {
+  echo 'Vous êtes connecté !<br/>'; 
+  header('location:emple.php');
+}
+else {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        echo 'mail invalide.<br/>';   
+    }
+    elseif (filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+      echo 'mot de passe invalide.<br/>';  
+    }
+}
+  $verif->closeCursor();   
+ 
+} 
 ?>
 
 <!DOCTYPE html>
@@ -40,25 +40,24 @@
     <link rel="stylesheet" href="../style/c_employe.css">
 </head>
 <body>
+  
     <div class="login">
 
         <h1 class="text-center">Bonjour</h1>
 
-        <form method='post' class="needs-validation">
-            <div class="form-group was-validated">
+        <form method='post' action="" class="needs-validation">
+            <div class="form-group ">
                 <label class="form-label" for="email">Adresse mail</label>
-                <input class="form-control" type="email" id="email" required>
+                <input class="form-control" type="text" id="email" name='email' required>
             </div>
-            <div class="form-group was-validated">
+            <div class="form-group ">
                 <label class="form-label" for="password">Mot de passe</label>
-                <input class="form-control" type="password" id="password" required>
-               
+                <input class="form-control" type="password" id="password" name='mdp' required>
             </div>
             <input class="btn" type="submit" value="S'identifier">
+
+
         </form>
-
     </div>
-
 </body>
-
 </html>
